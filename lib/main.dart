8f928d13298c17d9396c418327164f0c51084a25
Aarush Acharya <92685647+Aarush-Acharya@users.noreply.github.com>
2023-08-30 02:41:37 +0530

    refactor: entry point set to login screen

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class LoginApp extends StatelessWidget {
         useMaterial3: true,
       ),
       debugShowCheckedModeBanner: false,
-      home: admindashboard(),
+      home: LoginScreen(),
     );
   }
 }
